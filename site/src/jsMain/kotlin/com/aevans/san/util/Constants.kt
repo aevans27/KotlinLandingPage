adddb2f3ecf386e962ff3d73c237b2988b37d215
Allan Evans <oneblankpage27@gmail.com>
2024-06-05 16:41:58 -0600

    exported images for portfolio

--- site/src/jsMain/kotlin/com/aevans/san/util/Constants.kt
+++ site/src/jsMain/kotlin/com/aevans/san/util/Constants.kt
@@ -3,7 +3,7 @@ package com.aevans.san.util
 object Constants {
     const val SECTION_WIDTH = 1920
     const val FONT_FAMILY = "Roboto"
-    const val WEBSITE = "https://www.linkedin.com/in/allan-evans-2c/"
+    const val WEBSITE = "https://www.github.com/aevans27"
     const val LOREM_IPSUM_LONG =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const val LOREM_IPSUM_SHORTEST =
